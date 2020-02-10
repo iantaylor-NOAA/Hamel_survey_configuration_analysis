c705d1335816b237e07bfc861525b556a5d34437
Ian Taylor <ian.taylor@noaa.gov>
2020-02-10 15:26:55 -0800

    restore brighter colors for all but yellow & green

--- R/color_settings.R
+++ R/color_settings.R
@@ -1,3 +1,3 @@
 require(dichromat)
 col <- c(1,colorschemes$Categorical.12[seq(2,12,2)])
-col[-1] <- adjustcolor(col[-1], offset = c(-0.2, -0.2, -0.2, 0))
+col[3:4] <- adjustcolor(col[3:4], offset = c(-0.2, -0.2, -0.2, 0))
